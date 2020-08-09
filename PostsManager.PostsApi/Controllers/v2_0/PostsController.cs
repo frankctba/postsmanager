319326548809949045711c6e58f88a9fb9e070d0
Frank Cardoso <frank_cardoso@epam.com>
2020-08-09 14:52:42 +0200

    PostsManager - Development of the Posts Api

---
+++ PostsManager.PostsApi/Controllers/v2_0/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostsManager.PostsApi.Models.v1_0;
+using PostsManager.PostsApi.Repositories.v1_0;
+
+namespace PostsManager.PostsApi.Controllers.v2_0
+{
+    [ApiController]
+    [ApiVersion("2")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private IPostsRepository _postsRepository;
+
+        public PostsController(IPostsRepository postsRepository)
+        {
+            this._postsRepository = postsRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(PostsModel), StatusCodes.Status200OK)]
+        public ActionResult<PostsModel> GetAll()
+        {
+            return Ok(_postsRepository.GetPosts());
+        }
+    }
+}
---
+++ PostsManager.PostsApi/Repositories/v1_0/PostsRepository.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PostsManager.PostsApi.Models.v1_0;
+
+namespace PostsManager.PostsApi.Repositories.v1_0
+{
+    public class PostsRepository : IPostsRepository
+    {
+        private List<PostsModel> _posts;
+        private readonly object lockObject = new object();
+        
+        public PostsRepository()
+        {
+            this._posts = new List<PostsModel>();
+        }
+
+        public PostsModel Add(PostsModel post)
+        {
+            lock (lockObject)
+            {
+                post.Id = this._posts.Count <= 0 ? 1 : this._posts.Max(p => p.Id) + 1;
+                _posts.Add(post);
+                return post;
+            }
+        }
+
+        public PostsModel GetPostById(int postId)
+        {
+            lock (lockObject)
+            {
+                return _posts.SingleOrDefault(p => p.Id == postId);
+            }
+        }
+
+        public IEnumerable<PostsModel> GetPosts()
+        {
+            lock (lockObject)
+            {
+                return _posts;
+            }
+        }
+
+        public void Delete(int postId)
+        {
+            lock (lockObject)
+            {
+                var post = this.GetPostById(postId);
+                if (post != null)
+                    _posts.Remove(post);
+            }
+        }
+
+        public void Update(int postId, PostsModel post)
+        {
+            lock (lockObject)
+            {
+                var currentPost = this.GetPostById(postId);
+                if (currentPost != null)
+                {
+                    currentPost.Title = post.Title;
+                    currentPost.Description = post.Description;
+                }
+            }
+        }
+    }
+}
