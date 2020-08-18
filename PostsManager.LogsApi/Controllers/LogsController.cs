3e8e66ff9971968e87a547ea0a38eb66ca6cf99b
Frank Cardoso <frank_cardoso@epam.com>
2020-08-18 18:58:53 +0200

    LogsAPI, docker, containers (rabbitmq, elasticsearch)

---
+++ PostsManager.LogsApi/Controllers/LogsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PostsManager.LogsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogsController : ControllerBase
+    {
+        private readonly ILogger<LogsController> _logger;
+        public LogsController(ILogger<LogsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            _logger.LogInformation($"GetAll (Information) endpoint visited at {DateTime.UtcNow.ToLongTimeString()}");
+            _logger.LogError($"GetAll (Error) endpoint visited at {DateTime.UtcNow.ToLongTimeString()}");
+
+            return Ok();
+        }
+    }
+}
---
+++ PostsManager.LogsApi/Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace PostsManager.LogsApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(Configuration["ElasticConfiguration:Url"]))
+                {
+                    // AWS Connection
+                    //ModifyConnectionSettings = conn =>
+                    //{
+                    //    var httpConnection = new AwsHttpConnection(configuration.GetSection("AWS:Region").Value);
+                    //    var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("AWS:ElasticUrl").Value));
+                    //    var conf = new ConnectionConfiguration(pool, httpConnection);
+                    //    return conf;
+                    //},
+                    //IndexFormat = "emrelog-{0:yyyy.MM}",
+                })
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            loggerFactory.AddSerilog();
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Logs Api V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
